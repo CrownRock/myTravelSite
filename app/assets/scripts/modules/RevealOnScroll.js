52542b32afc296a52d02688808adcf8b1deff52b
Steve Stout <sstout7576@gmail.com>
2022-07-29 17:22:34 -0600

    Section15 -51 Reveal on Scroll - Testimonal Not Working

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,27 +1,51 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
 class RevealOnScroll {
-    constructor() {
-        this.itemsToReveal = document.querySelectorAll(".feature-item")
+    constructor(els, thresholdPercent) {
+        // this.itemsToReveal = document.querySelectorAll(".feature-item")
+        this.thresholdPercent = thresholdPercent
+        this.itemsToReveal = els
+        this.browserHeight = window.innerHeight
         this.hideInitially()
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
         this.events()
     }
 
     events() {
-        window.addEventListener("scroll", () => {
-            this.itemsToReveal.forEach(el => {
-                this.calculateIfScrolledTo(el)    
-            })
+        window.addEventListener("scroll", this.scrollThrottle)
+        window.addEventListener("resize", debounce(() => {
+            this.browserHeight = window.innerHeight
+        }, 333))
+    }
+
+    calcCaller() {
+        this.itemsToReveal.forEach(el => {
+            if (el.isRevealed == false) {
+                this.calculateIfScrolledTo(el)     
+            }       
         })
     }
 
     calculateIfScrolledTo(el) {
-        let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100
-        if (scrollPercent < 75) {
-            el.classList.add("reveal-item--is-visible")
+        if (window.scrollY + this.browserHeight > el.offsetTop) {
+            let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100
+            if (scrollPercent < this.thresholdPercent) {
+                el.classList.add("reveal-item--is-visible")
+                el.isRevealed = true
+                if (el.isLastItem) {
+                    window.removeEventListener("scroll", this.scrollThrottle)    
+                }
+            }
         }
     }
 
     hideInitially() {
-        this.itemsToReveal.forEach(el => el.classList.add("reveal-item"))
+        this.itemsToReveal.forEach(el => {
+            el.classList.add("reveal-item")
+            el.isRevealed = false
+        })
+        this.itemsToReveal[this.itemsToReveal.length -1].isLastItem = true
     }
 }
 
